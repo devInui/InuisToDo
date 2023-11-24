e57d913adb28d1fed1b4dbe4be072fdf9a77df0c
devInui <inuisoradev@gmail.com>
2023-11-25 08:32:22 +0900

    add Dnd modifiers

--- my-electron-app/src/web/SortableChildTasks.jsx
+++ my-electron-app/src/web/SortableChildTasks.jsx
@@ -22,30 +22,32 @@ function SortableChildTasks({
   isEvenOrder,
 }) {
   return (
-    <SortableContext
-      items={taskList.map((task) => task.taskId)}
-      strategy={verticalListSortingStrategy}
-    >
-      {taskList.map((task) => (
-        <Task
-          key={task.taskId}
-          task={task}
-          parentId={parentId}
-          centerPinTaskId={centerPinTaskId}
-          addChildTaskFront={addChildTaskFront}
-          addChildTaskEnd={addChildTaskEnd}
-          insertBrotherTask={insertBrotherTask}
-          deleteTask={deleteTask}
-          pinFlag={pinFlag}
-          toggleChecked={toggleChecked}
-          toggleClose={toggleClose}
-          switchSelect={switchSelect}
-          updateTaskName={updateTaskName}
-          revertLastChange={revertLastChange}
-          isEvenOrder={isEvenOrder}
-        />
-      ))}
-    </SortableContext>
+    <div>
+      <SortableContext
+        items={taskList.map((task) => task.taskId)}
+        strategy={verticalListSortingStrategy}
+      >
+        {taskList.map((task) => (
+          <Task
+            key={task.taskId}
+            task={task}
+            parentId={parentId}
+            centerPinTaskId={centerPinTaskId}
+            addChildTaskFront={addChildTaskFront}
+            addChildTaskEnd={addChildTaskEnd}
+            insertBrotherTask={insertBrotherTask}
+            deleteTask={deleteTask}
+            pinFlag={pinFlag}
+            toggleChecked={toggleChecked}
+            toggleClose={toggleClose}
+            switchSelect={switchSelect}
+            updateTaskName={updateTaskName}
+            revertLastChange={revertLastChange}
+            isEvenOrder={isEvenOrder}
+          />
+        ))}
+      </SortableContext>
+    </div>
   );
 }
 
